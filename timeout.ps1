d8f26c768a9a24cd9f053cc01938793021ffcddc
Lawis <139636331+Lawisx1@users.noreply.github.com>
2023-12-29 01:58:00 +0300

    Update timeout.ps1

--- timeout.ps1
+++ timeout.ps1
@@ -1,4 +1,4 @@
-while ($true) {
+jwhile ($true) {
     try {
         # Çalıştığını logla
         $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
@@ -10,12 +10,4 @@ while ($true) {
         # Belirli bir süre beklet
         Start-Sleep -Seconds 600
     }
-    catch {
-        # Hata oluşursa, hata bilgisini logla
-        $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-        Write-Host "$timestamp: An error occurred: $_"
-
-        # Hata sonrası bekleme süresi (opsiyonel)
-        Start-Sleep -Seconds 60
-    }
-}
+ 
