6588ec2c83c5d1eaa786ed4fe501176d68e9d4ca
Lawis <139636331+Lawisx1@users.noreply.github.com>
2023-12-28 20:27:33 +0300

    Update timeout.ps1

--- timeout.ps1
+++ timeout.ps1
@@ -1,4 +1,9 @@
 while ($true) {
-    Write-Host "Running..."
-    Start-Sleep -Seconds 600  # Her 10 dakikada bir kontrol et
+    try {
+        Write-Host "Running..."
+        Start-Sleep -Seconds 600  # Her 10 dakikada bir kontrol et
+    }
+    catch {
+        Write-Host "An error occurred: $_"
+    }
 }
