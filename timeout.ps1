5e2ce8decde7e44fb2e218b44afcb67b1bd49097
Lawis <139636331+Lawisx1@users.noreply.github.com>
2023-12-29 07:18:13 +0300

    Update timeout.ps1

--- timeout.ps1
+++ timeout.ps1
@@ -1,13 +1,9 @@
-#! /bin/bash
+#!/bin/bash
 
-case "$(pidof myprocess | wc -w)" in
+i=360
 
-0)  echo "Restarting My Process:     $(date)" >> /var/log/myprocess.txt
-    /home/pi/myprocess -and -some -args &
-    ;;
-1)  # all ok
-    ;;
-*)  echo "Removed double My Process: $(date)" >> /var/log/myprocess.txt
-    kill $(pidof myprocess | awk '{print $1}')
-    ;;
-esac
+while [ $i -gt 0 ]; do
+    echo $i
+    sleep 360
+    ((i--))
+done
